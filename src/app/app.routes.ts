872486c5ff6fd980d6c366e0b4e8ccb6bfdcf3bd
flaviodecruz <flaviocarey@yahoo.com.br>
2025-12-26 18:50:40 +0000

    Added Home page and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () =>
+      import('./home/home.page').then(m => m.HomePage)
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full',
-  },
+    pathMatch: 'full'
+  }
 ];
